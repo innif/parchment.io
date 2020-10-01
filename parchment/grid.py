8630329adb9486010440e8d7f617edf85ca6d7e7
FoxyChicken <wolf.hahn@charite.de>
2020-10-01 10:26:04 +0200

    removed square.py because dictionaries are better and I'm stupid

--- parchment/grid.py
+++ parchment/grid.py
@@ -1,1 +1,10 @@
-import square
+class Grid:
+	"""implements functions of a playing field for parchment.io"""
+	def __init__(self, x, y):
+		self.grid = []
+		row = []
+		for i in range(y):
+			for j in range(x):
+				row.append({"state": "empty"})
+			self.grid.append(row)
+			row = []
